9f86fcee3636c4a135c957c30883e80901663d2e
Jeong Kyu Lee <jkl499@nyu.edu>
2025-09-14 23:44:58 +0900

    Remove Hello Kitty icon from header
    - Remove Hello Kitty image from the header, keeping only '수업 일정' text
    - Clean up CSS by removing unused hello-kitty-icon styles and wave animation
    - Keep calendar icon (📅) in browser tab title
    - Simplify header layout for cleaner appearance

--- client/src/App.js
+++ client/src/App.js
@@ -165,14 +165,7 @@ function App() {
     <div className="app">
       <header className="app-header">
         <div className="header-content">
-          <h1>
-            <img 
-              src="/icon.png" 
-              alt="Hello Kitty" 
-              className="hello-kitty-icon"
-            />
-            수업 일정
-          </h1>
+          <h1>수업 일정</h1>
           <div className="header-actions">
             <button 
               className="action-button"
